17987e41109e4c005dc646e789e766bdddfb3bf5
RomanTIU <vip.polovei55@gmail.com>
2021-12-03 14:54:52 +0200

    Initial commit

---
+++ src/main/java/service/CurrencyConversionService.java
@@ -0,0 +1,14 @@
+package service;
+
+
+import entity.Currency;
+import service.impl.NbrbCurrencyConversionService;
+
+public interface CurrencyConversionService {
+
+    static CurrencyConversionService getInstance() {
+        return new NbrbCurrencyConversionService();
+    }
+
+    double getConversionRatio(Currency original, Currency target);
+}
